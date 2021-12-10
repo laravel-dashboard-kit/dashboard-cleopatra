6bbe5732d2176c9fb9a86a0dc70bb1382c7645f1
Amro Khaled <wshcylinder@gmail.com>
2021-12-10 17:18:45 +0200

    :zap:

--- resources/views/areas/horizontal/content.blade.php
+++ resources/views/areas/horizontal/content.blade.php
@@ -1,6 +1,9 @@
 <div class="bg-gray-100 flex-1 p-6 md:mt-16">
     {{-- <h1>{{ dashboard_ui()->title(__($attributes->get('title', 'Dashboard'))) }}</h1> --}}
-    {!! $slot !!}
 
-    @include('dashboard-abstract::include.notifications')
+    <div class="my-2">
+        @include('dashboard-abstract::include.notifications')
+    </div>
+
+    {!! $slot !!}
 </div>
