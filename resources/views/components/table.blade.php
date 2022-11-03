dda7a809bc71abd2087bbdbb09cc54873c1bd7b9
Amro Khaled <wshcylinder@gmail.com>
2022-11-03 11:30:02 +0200

    :zap: Table

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,32 +1,5 @@
-<x-dashboard-card :inset="true">
-    {{-- Table header --}}
-    @if ($attributes->get('title') || $attributes->get('subtitle') || isset($cardHeader))
-        <x-slot name="header">
-            @if ($attributes->get('title'))
-                {{ $attributes->get('title') }}
-            @endif
-
-            @if ($attributes->get('subtitle'))
-                {{ $attributes->get('subtitle') }}
-            @endif
-
-            @isset($cardHeader)
-                {!! $cardHeader !!}
-            @endisset
-        </x-slot>
-    @endif
-
-    {{-- table body --}}
-    <table {{ $attributes->merge([
-        'class' => 'table-auto w-full',
-    ]) }}>
-        {!! $slot !!}
-    </table>
-
-    {{-- table footer --}}
-    @isset($cardFooter)
-        <x-slot name="footer">
-            {!! $cardFooter !!}
-        </x-slot>
-    @endisset
-</x-dashboard-card>
+<table {{ $attributes->merge([
+    'class' => 'table-auto w-full',
+]) }}>
+    {!! $slot !!}
+</table>
