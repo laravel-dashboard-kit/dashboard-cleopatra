1746ea12624df2153a0f3972eb596ac7294c90d5
Amro Kamh <wshcylinder@gmail.com>
2024-05-12 14:34:59 +0300

    :fire: remove livewire datatable

--- resources/views/components/table/options.blade.php
+++ resources/views/components/table/options.blade.php
@@ -1,7 +1,7 @@
 <x-dashboard-table-body-col style="white-space: nowrap; width: 1%">
     <x-dashboard-dropdown outline color="dark">
         <x-slot name="title">
-            <x-tabler-dots-vertical />
+            <x-ri-arrow-drop-down-fill />
         </x-slot>
 
         {!! $slot !!}
--- resources/views/vendor/livewire/datatables/edit.blade.php
+++
@@ -1,5 +0,0 @@
-<x-admin-button color="success"
-    size="xs"
-    href="{{ route($formAction, $value) }}">
-    <x-feathericon-edit-2 />
-</x-admin-button>
