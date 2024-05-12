ed2f9a37dbebc068b1feda85ed9b22f83fa4e929
Amro Kamh <wshcylinder@gmail.com>
2024-05-12 14:04:47 +0300

    quick

--- resources/views/components/flex.blade.php
+++ resources/views/components/flex.blade.php
@@ -1,5 +1,10 @@
+@props([
+    'x' => null,
+    'y' => null,
+])
+
 <div {{ $attributes->merge([
-    'class' => "flex justify-${x} items-${y}",
+    'class' => "flex justify-{$x} items-{$y}",
     // 'dir' => dashboard_rtl('rtl', 'ltr')
 ]) }}>
     {!! $slot !!}
--- resources/views/components/table/header/col.blade.php
+++ resources/views/components/table/header/col.blade.php
@@ -2,7 +2,7 @@
 
 <th {{ $attributes->except('class')->merge([]) }}
     @class([
-        'px-4 py-2 font-normal ',
+        'px-4 py-2 font-normal ' . dashboard_rtl('text-right', 'text-left'),
         dashboard_rtl('border-r', 'border-l') => !$first,
         dashboard_rtl('border-l-0', 'border-r-0') => $last,
     ])>
